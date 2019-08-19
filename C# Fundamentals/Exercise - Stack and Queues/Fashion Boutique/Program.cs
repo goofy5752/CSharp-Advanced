b57448bd8b1ea2862a54f617ae51b7f73d2c1816
Martin Asenov <m.asenov5752@gmail.com>
2019-08-19 16:22:38 +0300

    c

---
+++ C# Fundamentals/Exercise - Stack and Queues/Fashion Boutique/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int newRack = 0;
+            var stack = new Stack<int>();
+
+            int[] clothesWeight = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < clothesWeight.Length; i++)
+            {
+                stack.Push(clothesWeight[i]);
+            }
+
+            int totalRacks = 0;
+
+            for (int i = 0; i < clothesWeight.Length; i++)
+            {
+                newRack += stack.Pop();
+                if (newRack >= rackCapacity || (newRack + stack.Peek()) >= rackCapacity)
+                {
+                    if ((newRack + stack.Peek()) >= rackCapacity)
+                    {
+                        newRack = 0;
+                    }
+                    totalRacks++;
+                }
+            }
+
+            Console.WriteLine(totalRacks);
+        }
+    }
+}
