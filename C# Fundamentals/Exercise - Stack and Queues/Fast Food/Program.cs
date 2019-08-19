1032ffe3fc336563246b3e82d11871fe232076e7
Martin Asenov <m.asenov5752@gmail.com>
2019-08-19 11:54:05 +0300

    changes

---
+++ C# Fundamentals/Exercise - Stack and Queues/Fast Food/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<int>();
+
+            int foodCount = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            int[] foodInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var t in foodInfo)
+            {
+                queue.Enqueue(t);
+            }
+
+            int queueCount = queue.Count;
+            int queueLargestNumber = queue.Max();
+
+            for (int i = 0; i < queueCount; i++)
+            {
+                if (foodCount < queue.Peek())
+                {
+                    Console.WriteLine(queueLargestNumber);
+                    Console.WriteLine($"Orders left: {string.Join(',', queue)}");
+                    break;
+                }
+
+                foodCount -= queue.Dequeue();
+
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine(queueLargestNumber);
+                    Console.WriteLine("Orders complete");
+                }
+            }
+        }
+    }
+}
